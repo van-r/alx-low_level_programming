bcf3e0382dce488d9bc9bc9246ff31f83a950305
van-r <vanessarugami@gmail.com>
2023-02-18 12:59:41 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - First function to run
+ *
+ * Return: 1 for error
+ */
+int main(void)
+{
+		fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
