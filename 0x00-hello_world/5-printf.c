d146af19a79ccf993aceaef4e90177ab58afe437
van-r <vanessarugami@gmail.com>
2023-02-18 12:53:15 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - First function to run
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
